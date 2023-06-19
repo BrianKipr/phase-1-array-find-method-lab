87ed97a3e68ef9541409098fda8c270e788f3275
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-19 12:26:55 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record){
+    const superbowlWin = ('returns a year the Denver Broncos won the superbowl') 
+        console.log(superbowlWin)
+}
+function sadReality(result){
+const undefined = ('2015')
+        console.log(undefined)
+}
